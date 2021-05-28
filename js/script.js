3e3a9cce7b39b528abfd251d4a0c18dcd326ed2a
Elena <lunorija1988@mail.ru>
2021-05-28 15:06:22 +0300

    first js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.isMarried);
+
+confirm("are you here?");
+
+let incr = 10,
+    decr = 10;
+
+incr ++;
+decr--;
+
+console.log(incr);
+console.log(decr);
